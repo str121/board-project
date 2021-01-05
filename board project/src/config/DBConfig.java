e060c24111a0e75fc21301e996db026fbee4a4c0
str121 <poilk2447@gmail.com>
2021-01-05 12:51:51 +0900

    페이지접속

---
+++ board project/src/config/DBConfig.java
@@ -0,0 +1,7 @@
+package config;
+public interface DBConfig {
+	public String DB_DRIVER = "oracle.jdbc.OracleDriver";
+	public String DB_URL = "jdbc:oracle:thin:@175.197.213.135:32769:xe";
+	public String DB_USER = "scott";
+	public String DB_PASS= "tiger";
+}
